5765dfadaa2535267e9d535c38f283787f881227
MeisterBill <arthurvde@hotmail.fr>
2023-09-06 18:18:28 +0200

    actually done

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -2,28 +2,23 @@
 
 int main(int ac, char **argv)
 {
-	int i;
-	int j;
-
-	i = 1;
-	j = 0;
-	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE. *" << std::endl;
-		return (0);
-	}
-	while (i < ac)
-	{
-		j = 0;
-		while (argv[i][j] != '\0')
-		{
-			std::cout << (char)(std::toupper(argv[i][j]));
-			j++;
-		}
-		if (i != ac - 1)
-			std::cout << " ";
-		i++;
-	}
-	std::cout << std::endl;
-	return (0);
+    if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    }
+    else
+    {
+        int i = 1;
+        while (argv[i])
+        {
+            int j = 0;
+            while (argv[i][j])
+            {
+                std::cout << (char)toupper(argv[i][j]);
+                j++;
+            }
+            i++;
+        }
+    }
+    std::cout << std::endl;
 }
