62dd310829967918515bb467bd3ee1fdbe5ddfd0
MeisterBill <arthurvde@hotmail.fr>
2023-09-10 20:25:26 +0200

    DAY OVER :) , ex02-03 done

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -5,20 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: artvan-d <artvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/10 13:54:16 by artvan-d          #+#    #+#             */
-/*   Updated: 2023/09/10 17:07:09 by artvan-d         ###   ########.fr       */
+/*   Created: 2023/09/10 15:14:20 by artvan-d          #+#    #+#             */
+/*   Updated: 2023/09/10 17:50:21 by artvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include <iostream>
 
 int main()
 {
-    Zombie *bill = newZombie("Bill");
+    std::string str_brain("HI THIS IS BRAIN");
+    std::string *stringPTR = &str_brain;
+    std::string &stringREF = str_brain;
 
-    bill->announce();
-    randomChump("Georges");
-	randomChump("Lucas");
-	randomChump("Martin");
-    delete bill;
+    std::cout << "string address : " << &str_brain << std::endl;
+    std::cout << "value of stringPTR : " << stringPTR << std::endl;
+    std::cout << "address of stringREF : " << &stringREF << std::endl;
+
+    std::cout << "string value : " << str_brain << std::endl;
+    std::cout << "value pointed by stringPTR : " << *stringPTR << std::endl;
+    std::cout << "value of stringREF : " << stringREF << std::endl;
 }
