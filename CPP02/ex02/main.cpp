0ee832c17bbd76c059948180d51241e21e5328d3
MeisterBill <arthurvde@hotmail.fr>
2023-09-14 17:50:02 +0200

    changed main to same as subject

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,22 @@
 /*   By: artvan-d <artvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 17:36:33 by artvan-d          #+#    #+#             */
-/*   Updated: 2023/09/14 17:36:34 by artvan-d         ###   ########.fr       */
+/*   Updated: 2023/09/14 17:49:24 by artvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int	main( void ) 
+{ 
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0; 
+}
