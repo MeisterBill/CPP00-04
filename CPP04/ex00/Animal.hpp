1351aff504aef1ee4635c5f85fa3824c6db7da9c
Arthur van der Elst <arthurvde@hotmail.fr>
2023-09-19 16:47:30 +0200

    animal done

---
+++ CPP04/ex00/Animal.hpp
@@ -0,0 +1,26 @@
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <string>
+# include <iostream>
+
+class Animal
+{
+
+	protected:
+
+	std::string _type;
+
+	public:
+
+	Animal();
+	Animal(const Animal &other);
+	virtual ~Animal();
+	Animal &operator=(const Animal &other);
+
+	std::string	getType(void)	const;
+
+	virtual	void	makeSound(void)	const;
+};
+
+#endif
---
+++ CPP04/ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int main()
+{
+	const Animal	*animal = new Animal();
+	const Animal	*a_dog = new Dog();
+	const Animal	*a_cat = new Cat();
+	std::cout << a_dog->getType() << " " << std::endl;
+	std::cout << a_cat->getType() << " " << std::endl;
+	a_dog->makeSound(); // will output the dog sound!
+	a_cat->makeSound();
+	animal->makeSound();
+
+	
+
+	return (0);
+}
